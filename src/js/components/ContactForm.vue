c841d218dd7681fea666cc6aa1d974f52ccb5341
medved_v_ushanke <getingjagare@gmail.com>
2019-02-04 02:39:11 +0300

    new languages

--- src/js/components/ContactForm.vue
+++ src/js/components/ContactForm.vue
@@ -9,21 +9,21 @@
                         height="3px"></b-progress>
         </div>
         <b-form-group class="contact-form__field">
-            <b-form-input placeholder="Имя" required v-model="form.name" class="contact-form__input"
-                          autocomplete="off"></b-form-input>
+            <b-form-input :placeholder="nameText" required v-model="form.name" class="contact-form__input"
+                          autocomplete="off" :title="fillText"></b-form-input>
         </b-form-group>
         <b-form-group class="contact-form__field">
             <b-form-input placeholder="E-mail" type="email" required v-model="form.email"
-                          class="contact-form__input" autocomplete="off"></b-form-input>
+                          class="contact-form__input" autocomplete="off" :title="fillText"></b-form-input>
         </b-form-group>
         <b-form-group class="contact-form__field">
-            <b-form-textarea v-model="form.text" placeholder="Ваш вопрос" :rows="3" :max-rows="6" required
-                             class="contact-form__input" autocomplete="off"></b-form-textarea>
+            <b-form-textarea v-model="form.text" :placeholder="messageText" :rows="3" :max-rows="6" required
+                             class="contact-form__input" autocomplete="off" :title="fillText"></b-form-textarea>
         </b-form-group>
         <b-form-group class="contact-form__field contact-form__field_left">
             <vue-recaptcha sitekey="6LfoGIkUAAAAABynlLPkWxTcUj1X9dpYWYGtF6FA" size="compact" @expired="resetCaptcha">
                 <button type="submit" class="btn btn-secondary contact-form__submit">
-                    <span v-bind:class="{'contact-form__submit-text': true, 'contact-form__submit-text_pale': submitted}">Отправить</span>
+                    <span v-bind:class="{'contact-form__submit-text': true, 'contact-form__submit-text_pale': submitted}">{{ sendText }}</span>
                     <vue-loading :active="submitted" color="#fff" spinner="ring"></vue-loading>
                 </button>
             </vue-recaptcha>
@@ -50,6 +50,16 @@
                     text: '',
                     email: ''
                 }
+            }
+        },
+
+        props: ['nameText', 'messageText', 'sendText', 'fillText'],
+
+        mounted () {
+            var requiredFields = this.$el.querySelectorAll('input[required], textarea[required]');
+
+            for (var i = 0; i < requiredFields.length; i++) {
+                requiredFields[i].setCustomValidity(this.fillText);
             }
         },
 
