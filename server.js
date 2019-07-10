644c27cda8a682914de2e80bf80b3462c7e86b31
medved_v_ushanke <getingjagare@gmail.com>
2019-07-11 01:29:24 +0300

    fixes

--- server.js
+++ server.js
@@ -8,7 +8,29 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const router = require('./router')(app);
+app.post('/mail', function (req, res) {
+    const shell = require('shelljs');
+
+    if (!req.body['g-recaptcha-response'].length) {
+        res.write(JSON.stringify({success: 0, message: 'Капча не пройдена!'}));
+        res.end();
+    }
+
+    const {name, email, text} = req.body;
+    if (!name || !email || !text) {
+        res.write(JSON.stringify({success: 0, message: 'Не все поля заполнены'}));
+        res.end();
+    }
+
+    const from = 'noreply@black-freak-society.ru';
+    const to = 'mbd.kloster@yandex.ru';
+    const subject = 'Сообщение с сайта';
+    shell.exec('echo "From: ' + from + '\r\nSubject: ' + subject + '\r\nTo: ' + to + '\r\n\r\nИмя: ' + name + '\r\n' +
+        'Email: ' + email + '\r\nСообщение: ' + text + '" | sendmail -f ' + from + ' ' + to);
+
+    res.write(JSON.stringify({success: 1, message: 'Сообщение отправлено'}));
+    res.end();
+});
 
 app.listen(setup.port, function () {
     console.log('Server running at 127.0.0.1:%s', setup.port);
