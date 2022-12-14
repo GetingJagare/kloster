143db023cd306562cc5e1783007a2bed66f1949f
Nikita <getingjagare@gmail.com>
2022-12-14 17:39:10 +0100

    nginx config example && deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git checkout old
 git merge origin/old
 
 sudo docker compose down
-sudo docker image rm kloster2-backend:latest --force
+sudo docker image rm kloster-backend:latest --force
 sudo docker compose up -d
