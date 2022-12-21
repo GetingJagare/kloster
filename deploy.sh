47abff609fb879957307966025fe3a3f40fa661c
Nikita <getingjagare@gmail.com>
2022-12-21 01:22:02 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ git merge origin/old
 sudo docker compose down
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
-sudo docker compose up -d
 sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d vsv-kloster.ru -d www.vsv-kloster.ru
+sudo docker compose up -d
