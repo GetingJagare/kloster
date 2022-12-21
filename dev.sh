eefc9809b17e65deb89fce0c048fca67bb7fb206
Nikita <getingjagare@gmail.com>
2022-12-21 01:34:47 +0100

    dev script

--- dev.sh
+++ dev.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git fetch
+git checkout old
+git merge origin/old
+
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
 sudo docker compose -f docker-compose-dev.yml up -d
