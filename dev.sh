98250e92155307f16af005e4479906193f145815
Nikita <getingjagare@gmail.com>
2022-12-21 01:22:44 +0100

    dev script

--- dev.sh
+++ dev.sh
@@ -2,4 +2,4 @@
 
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
-sudo docker compose -f docker-compose-dev.yml up
+sudo docker compose -f docker-compose-dev.yml up -d
