396b0cfb41d836092fcb31ade55624e7860e32c3
Nikita <getingjagare@gmail.com>
2022-12-21 02:11:16 +0100

    prod & dev

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,4 @@ git merge origin/old
 
 sudo docker compose down
 sudo docker image rm kloster-backend:latest --force
-sudo docker image rm kloster-nginx:latest --force
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d vsv-kloster.ru -d www.vsv-kloster.ru
 sudo docker compose up -d
--- dev.sh
+++ dev.sh
@@ -6,4 +6,4 @@ git merge origin/old
 
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
-sudo docker compose -f docker-compose-dev.yml up -d
+sudo docker compose -f docker-compose-dev.yml up
