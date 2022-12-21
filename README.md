16a35131ae030181ff0f9b19e9cbb44d6d854c87
Nikita <getingjagare@gmail.com>
2022-12-21 01:15:27 +0100

    refactoring

--- README.md
+++
@@ -1,5 +0,0 @@
-### Certificates for development mode
-```bash
-mkdir dev-certs
-openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout dev-certs/kloster.key -out dev-certs/kloster.crt
-```
--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,3 @@ sudo docker compose down
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
 sudo docker compose up -d
-sudo docker compose run --rm certbot certonly -d vsv-kloster.ru -d www.vsv-kloster.ru
