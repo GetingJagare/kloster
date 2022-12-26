5a670ae3d18c73da8bdb881aee10aa821868f76a
Nikita <getingjagare@gmail.com>
2022-12-26 20:56:19 +0100

    mail

--- backend/app/server.js
+++ backend/app/server.js
@@ -24,18 +24,18 @@ app.post('/mail', function (req, res) {
             res.write(JSON.stringify({success: 0, errorCode: 2}));
         } else {
             const shell = require('shelljs'),
-                from = 'noreply@vsv-kloster.ru',
-                to = 'mbd.kloster@yandex.ru,getingjagare@gmail.com',
-                subject = 'Сообщение с сайта',
-                date = new Date();
-            shell.exec(`echo "Date: ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}\r\n` +
-                `---------------------------\r\n` +
-                `From: ${from}\r\nSubject: ${subject}\r\nTo: ${to}\r\n\r\nИмя: ${name}\r\n` +
-                `Email: ${email}\r\nСообщение: ${text}\r\n` +
-                `---------------------------\r\n" >> mail/mail.txt`);
-            /**
-             * echo "From: noreply@vsv-kloster.ru\r\nSubject: Сообщение с сайта\r\nTo: mbd.kloster@yandex.ru,getingjagare@gmail.com\r\n\r\nИмя: test\r\nEmail: test@test.com\r\nСообщение: test text" | sendmail -f noreply@vsv-kloster.ru mbd.kloster@yandex.ru,getingjagare@gmail.com
-             */
+                moment = require('moment'),
+                {MAIL_FROM: from, MAIL_TO: to, MAIL_HOST: host, MAIL_USER: user, MAIL_PASSWORD: password} = process.env,
+                subject = 'Сообщение с сайта vsv-kloster.ru',
+                date = moment().format("ddd, DD MMM Y H:mm:s ZZ"),
+                fullMessage = `Имя: ${name}\r\nEmail: ${email}\r\nСообщение: ${text}\r\n`,
+                fullEmail = `Date: ${date}\r\nFrom: ${from}\r\nSubject: ${subject}\r\nTo: ${to}\r\n\r\n${fullMessage}\r\n`;
+            shell.exec(`echo -e "${fullEmail}" >> mail/mail.txt`);
+            shell.exec(`cat /dev/null > mail/email.txt`);
+            shell.exec(`echo -e "${fullEmail}" >> mail/email.txt`);
+            shell.exec(`curl --ssl smtp://${host} --mail-from ${from}` +
+                ` ${to.trim().split(/\s*,\s*/).map((mailbox) => `--mail-rcpt ${mailbox}`).join(' ')}` +
+                ` --upload-file mail/email.txt --user '${user}:${password}'`);
 
             res.write(JSON.stringify({success: 1, message: 'Сообщение отправлено'}));
         }
--- dev.sh
+++ dev.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-git fetch
-git checkout old
-git merge origin/old
-
 sudo docker image rm kloster-backend:latest --force
 sudo docker image rm kloster-nginx:latest --force
 sudo docker compose -f docker-compose-dev.yml up
