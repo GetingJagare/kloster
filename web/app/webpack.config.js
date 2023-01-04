3781169f8eb0a92318cb9313eccf4afcd58a36a1
Nikita <getingjagare@gmail.com>
2023-01-04 16:57:21 +0100

    frontend refactoring

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -71,29 +71,24 @@ export default {
     module: {
         rules: [
             {
-                test: /\.(s*)css$/i,
+                test: /\.s?css$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
+                    'css-loader',
                     {
-                        loader: "css-loader",
+                        loader: "sass-loader",
                         options: {
                             sourceMap: true,
-                        },
-                    },
-                    "sass-loader",
+                        }
+                    }
                 ],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)(.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[hash].[ext]',
-                            esModule: false,
-                        }
-                    }
-                ],
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]'
+                }
             },
             {
                 test: /\.js$/,
@@ -101,31 +96,10 @@ export default {
             },
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: true,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.9],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        },
-                    },
-                ],
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]'
+                }
             },
             {
                 test: /\.vue$/,
